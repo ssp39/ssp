b44ee2b49f59df270dc1ba4dcdc9519c8d2d220e
ssp39 <56770762+ssp39@users.noreply.github.com>
2019-10-19 19:25:04 +0100

    Add files via upload

---
+++ Euler Project.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 18:40:43 2019
+
+@author: siddhiparekh
+"""
+
+# problem 1
+        
+emptyList = []
+for x in range(1000):
+    if x% 3 == 0 or x % 5 ==0:
+        emptyList.append(x)
+sum(emptyList)
+
+# problem 2
+import sys
+
+emptyList2 = [1, 2]
+for x in range(2, sys.maxsize):
+    newValue = emptyList2[x-1] + emptyList2[x-2]
+    emptyList2.append(newValue)
+    if newValue > 4000000:
+        break
+
+newList = [x for x in emptyList2 if x % 2 == 0 and x < 4000000]
+sum(newList)
+
+# problem 3
